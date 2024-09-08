4165031490d5fda43de4f1ee53a8afcd175a8df9
Akshat Bajetha <akshatbajetha@gmail.com>
2024-09-08 21:59:15 +0530

    Minor edit

--- src/main.jsx
+++ src/main.jsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
     <Analytics />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
