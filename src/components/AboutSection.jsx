f7a83763d1b3ff91c94ccb1b41bf261bcbe95018
Akshat Bajetha <akshatbajetha@gmail.com>
2024-09-08 17:45:02 +0530

    Edit few lines in home and about page

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -9,10 +9,10 @@ function AboutSection() {
     <>
       <div className='flex justify-center w-full about-me intro'>
         <div className='dark:text-text-colour text-black-text '>
-        Hi guys, I am <span className='blue-gradient'>Akshat Bajetha</span> from Delhi, India, currently in my third year of Computer Science and Engineering from <span className='orange-gradient'>Bennett University</span>. I'm a self-taught web developer currently learning MERN stack. I have a passion for learning and creating new things.
+        Hi guys, I am <span className='blue-gradient'>Akshat Bajetha</span> from Delhi, India, currently in my third year of Computer Science and Engineering from <span className='orange-gradient'>Bennett University</span>. I'm a self-taught web developer, currently learning MERN stack, who loves problem solving as well. I have a passion for learning and creating new things.
         </div>
         <div>
-          <img src={MyPhoto} className='my-photo' alt="Avatar" width={"200px"} />
+          <img src={MyPhoto} className='my-photo' alt="Avatar" width={"700px"} />
         </div>
       </div>
       <div className='text-center logo-carousel-container'>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
       exit={{ x: 100, opacity: 0 }}
     className='w-full h-screen flex overflow-y-hidden flex-row items-center justify-center home-page align-middle bg-white-background dark:bg-background'>
       
-      <div className='flex justify-center items-center align-middle flex-col home-page'>
+      <div className='flex justify-center items-center align-middle flex-col home-page-text'>
           <div className='text-center mb-2'>
             <h1 className='dark:text-text-colour text-black-text heading font-bold'>Hi, I am <span className='gradient'>Akshat Bajetha</span></h1>
           </div>
@@ -30,7 +30,7 @@ function Home() {
             <h3><span className='dark:text-text-colour text-black-text subheading font-bold'>I am <span className='blue-gradient'>{text}</span></span></h3>
           </div>
           <div className='text-center mb-2 brief flex'>
-            <p className='dark:text-off-text-colour text-off-black home-text'>I am a frontend developer. I am currently learning full stack development (MERN) and am interested in learning new technologies.</p>
+            <p className='dark:text-off-text-colour text-off-black home-text'>A self-taught frontend developer, currently learning full stack development (MERN). I am interested in exploring and learning new technologies.</p>
           </div>
             <Button text={'View Resume'} />
       </div>
